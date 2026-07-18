a7da52c06f8c43cbb1b594569b5626949b151160
randy2014 <randy2014@users.noreply.github.com>
2026-07-18 12:59:44 +0800

    fix: 添加 ApplicationRunner 初始化默认管理员

--- video-entitlement-server/src/main/java/com/video/entitlement/VideoEntitlementApplication.java
+++ video-entitlement-server/src/main/java/com/video/entitlement/VideoEntitlementApplication.java
@@ -1,11 +1,19 @@
 package com.video.entitlement;
 
+import com.video.entitlement.module.admin.service.AdminService;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VideoEntitlementApplication {
     public static void main(String[] args) {
         SpringApplication.run(VideoEntitlementApplication.class, args);
+    }
+
+    @Bean
+    ApplicationRunner initDefaultAdmin(AdminService adminService) {
+        return args -> adminService.initDefaultAdmin();
     }
 }
